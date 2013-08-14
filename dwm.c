e3d8840185003e999de1f4f2fd5a0f690295d8f2
Sven Schober <sven.schober@uni-ulm.de>
2013-08-14 10:05:18 +0200

    shiftview patch

--- dwm.c
+++ dwm.c
@@ -2453,6 +2453,26 @@ zoom(const Arg *arg) {
 	pop(c);
 }
 
+/** Function to shift the current view to the left/right
+ *
+ * @param: "arg->i" stores the number of tags to shift right (positive value)
+ *          or left (negative value)
+ */
+void
+shiftview(const Arg *arg) {
+        Arg shifted;
+
+        if(arg->i > 0) // left circular shift
+                shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
+                   | (selmon->tagset[selmon->seltags] >> (LENGTH(tags) - arg->i));
+
+        else // right circular shift
+                shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
+                   | selmon->tagset[selmon->seltags] << (LENGTH(tags) + arg->i);
+
+        view(&shifted);
+}
+
 int
 main(int argc, char *argv[]) {
 	if(argc == 2 && !strcmp("-v", argv[1]))
